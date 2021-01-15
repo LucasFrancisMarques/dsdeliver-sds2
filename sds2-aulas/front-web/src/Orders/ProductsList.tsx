1d96e1ed273f52e908b0034ec493fa2d9b1aa261
Lucas Silva <lucasfrancismarques@gmail.com>
2021-01-15 04:14:28 -0300

    Add routes and StepsHeader

---
+++ sds2-aulas/front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,11 @@
+function ProductsList() {
+    return (
+        <div className="orders-list-container">
+            <div className="orders-list-content">
+                Products list
+            </div>   
+        </div>
+    )
+}
+
+export default ProductsList;
--- sds2-aulas/front-web/src/Orders/index.tsx
+++ sds2-aulas/front-web/src/Orders/index.tsx
@@ -1,10 +1,12 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
 
 function Orders() {
     return (
-        <div className="ordes-cantainer">
+        <div className="orders-container">
             <StepsHeader/>
+            <ProductsList/>
         </div>
     )
 }
